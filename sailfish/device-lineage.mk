f3d3a815303659eac5cef1fb30173245bf798809
hairycheez <22867901+hairycheez@users.noreply.github.com>
2020-08-25 18:11:34 -0400

    Revert "Pixel Gapps"
    This reverts commit 2cbf48484ab332dce495e8fb08997b85287ac57a.

--- sailfish/device-lineage.mk
+++ sailfish/device-lineage.mk
@@ -1,7 +1,1 @@
-# Inherit some common PixelExperience stuff
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-
-$(call inherit-product, vendor/gapps/config.mk)
 $(call inherit-product, device/google/marlin/device-lineage.mk)
